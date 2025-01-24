aa5ee071954eb277b5cdc49b992f35bb4a6df164
Franz Rosete <frosete@myseneca.ca>
2025-01-24 02:01:22 -0500

    completed task

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+##Author: Franz Rosete
+##AuthorID: Frosete 157712183
+##Date Created: 2025-01-24
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')    
